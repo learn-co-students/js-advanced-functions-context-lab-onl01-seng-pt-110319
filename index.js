9a6333a890b2bcb784fcba8c63d01f80976e873c
stephensanchez05 <stephensanchez05@gmail.com>
2020-07-02 20:47:16 -0400

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,59 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(answer) {
+    return {
+        firstName: answer[0],
+        familyName: answer[1],
+        title: answer[2],
+        payPerHour: answer[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(answer) {
+    const map1 = answer.map(x => createEmployeeRecord(x));
+    return map1
+}
+
+function createTimeInEvent(employee) {
+    let [day, hour] = employee.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: day,
+    })
+    return this
+}
+
+function createTimeOutEvent(employee) {
+    let [day, hour] = employee.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: day,
+    })
+    return this
+}
+
+function hoursWorkedOnDate(day) {
+    let i = this.timeInEvents.find(function(x){
+        return x.date === day
+    })
+
+    let b = this.timeOutEvents.find(function(x){
+        return x.date === day
+    })
+
+    const answer = (b.hour - i.hour) / 100
+    return answer
+
+}
+
+function wagesEarnedOnDate(day) {
+    return parseInt((hoursWorkedOnDate.call(this, day) * this.payPerHour), 10)
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,3 +73,15 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
+  }
+  
+  let calculatePayroll = function(arrayOfEmployeeRecords){
+      return arrayOfEmployeeRecords.reduce(function(memo, rec){
+          return memo + allWagesFor.call(rec)
+      }, 0)
+  }
