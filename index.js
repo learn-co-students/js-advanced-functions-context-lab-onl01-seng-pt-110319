81f1fcd8a7f1520be99e2f8b9f80bd9fed1ad7bc
github username <github email address>
2021-01-12 22:29:27 -0500

    Done.

--- index.js
+++ index.js
@@ -22,111 +22,111 @@ function createEmployeeRecord(employee) {
 }
 
 
-// function createEmployeeRecords(array) {
-//    return array.map(employee => createEmployeeRecord(employee))
-// }
+function createEmployeeRecords(array) {
+   return array.map(employee => createEmployeeRecord(employee))
+}
 
-// function createTimeInEvent(date) {
-//    let event = {
-//        type: "TimeIn", 
-//        hour: parseInt(date.split(" ")[1], 10), 
-//        date: date.split(" ")[0]
-//    }
-//     this.timeInEvents.push(event)
-//     return this
-// }
+function createTimeInEvent(date) {
+   let event = {
+       type: "TimeIn", 
+       hour: parseInt(date.split(" ")[1], 10), 
+       date: date.split(" ")[0]
+   }
+    this.timeInEvents.push(event)
+    return this
+}
 
-// function hoursWorkedOnDate(date) {
+function hoursWorkedOnDate(date) {
     
-//     let timeIn = this.timeInEvents.filter(d => d.date === date)[0]
-//     let timeOut = this.timeOutEvents.filter(d => d.date===date)[0]
+    let timeIn = this.timeInEvents.filter(d => d.date === date)[0]
+    let timeOut = this.timeOutEvents.filter(d => d.date===date)[0]
     
 
-//     return (timeOut.hour - timeIn.hour)/100
-// }
+    return (timeOut.hour - timeIn.hour)/100
+}
 
-// function wagesEarnedOnDate(date) {
-//     // let pay =  this.hoursWorkedOnDate(date).reduce((h, total) => total += h) * this.payPerHour
-//     return hoursWorkedOnDate.call(this, date) * this.payPerHour
+function wagesEarnedOnDate(date) {
+    // let pay =  this.hoursWorkedOnDate(date).reduce((h, total) => total += h) * this.payPerHour
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
     
-// }
+}
 
-// function createTimeOutEvent(date) {
+function createTimeOutEvent(date) {
 
-//     let event = {
-//         type: "TimeOut", 
-//         hour: parseInt(date.split(" ")[1], 10), 
-//         date: date.split(" ")[0]
-//     }
-//      this.timeOutEvents.push(event)
-//      return this
-// }
-// function findEmployeeByFirstName(collection, firstNameString) {
+    let event = {
+        type: "TimeOut", 
+        hour: parseInt(date.split(" ")[1], 10), 
+        date: date.split(" ")[0]
+    }
+     this.timeOutEvents.push(event)
+     return this
+}
+function findEmployeeByFirstName(collection, firstNameString) {
     
     
-//     let employee = collection.filter(d => d.firstName === firstNameString)[0]
+    let employee = collection.filter(d => d.firstName === firstNameString)[0]
     
-//     return employee
+    return employee
 
-// }
-// function calculatePayroll(employees) {
-//    return employees.reduce((total, e) => allWagesFor.call(e) + total, 0)
-// }
-
-
-// function createEmployeeRecord(ary){
-//     let record
-//     return record = { 
-//         firstName: ary[0],
-//         familyName: ary[1],
-//         title: ary[2],
-//         payPerHour: ary[3],
-//         timeInEvents: [], 
-//         timeOutEvents: []
-//     }
-// }
-
-// function createEmployeeRecords(arys){
-//     return arys.map(createEmployeeRecord)
-// }
-
-// function createDSObj(getType, dateStamp) {
-//     return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
-// }
-
-// function createTimeInEvent(dateStamp){
-//     this.timeInEvents.push(createDSObj("TimeIn", dateStamp))
-//     return this
-// }
+}
+function calculatePayroll(employees) {
+   return employees.reduce((total, e) => allWagesFor.call(e) + total, 0)
+}
 
 
-// function createTimeOutEvent(dateStamp){
-//     this.timeOutEvents.push(createDSObj("TimeOut", dateStamp))
-//     return this
-// }
+function createEmployeeRecord(ary){
+    let record
+    return record = { 
+        firstName: ary[0],
+        familyName: ary[1],
+        title: ary[2],
+        payPerHour: ary[3],
+        timeInEvents: [], 
+        timeOutEvents: []
+    }
+}
 
-// function hoursWorkedOnDate(employee,dateYMD){
-//     const timeIn = employee.timeInEvents.find((e) => e.date === dateYMD).hour
-//     const timeOut = employee.timeOutEvents.find((e) => e.date === dateYMD).hour
-//     return (timeOut - timeIn)/100
-// }
+function createEmployeeRecords(arys){
+    return arys.map(createEmployeeRecord)
+}
 
-// function wagesEarnedOnDate(date) {
-//     return hoursWorkedOnDate.call(this, date) * this.payPerHour
-// }
+function createDSObj(getType, dateStamp) {
+    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
 
-// // break
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push(createDSObj("TimeIn", dateStamp))
+    return this
+}
 
-// let allWagesFor = function () {
-//     let eligibleDates = this.timeInEvents.map(function (e) {
-//         return e.date
-//     })
 
-//     let payable = eligibleDates.reduce(function (memo, d) {
-//         return memo + wagesEarnedOnDate.call(this, d)
-//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push(createDSObj("TimeOut", dateStamp))
+    return this
+}
 
-//     return payable
-// }
+function hoursWorkedOnDate(employee,dateYMD){
+    const timeIn = employee.timeInEvents.find((e) => e.date === dateYMD).hour
+    const timeOut = employee.timeOutEvents.find((e) => e.date === dateYMD).hour
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
 
 // break
+
+let allWagesFor = function () {
+    let eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
+
+break
